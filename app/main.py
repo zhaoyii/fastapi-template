0be84bd6eac00b26894f37039e1b3217c3c671b2
andrew.zhao <andrew.zhao@wayz.ai>
2023-12-04 13:45:08 +0800

    add metadata lifespan

--- app/main.py
+++ app/main.py
@@ -1,11 +1,54 @@
 from typing import Union
 from fastapi import FastAPI
 from dotenv import load_dotenv
+from contextlib import asynccontextmanager
 
 # take environment variables from .env.
-load_dotenv()  
+load_dotenv()
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # startup event
+    print("startup event")
+    
+    yield
+
+    # shutdown event
+    print("shutdown event")
+
+description = """
+ChimichangApp API helps you do awesome stuff. 🚀
+
+## Items
+
+You can **read items**.
+
+## Users
+
+You will be able to:
+
+* **Create users** (_not implemented_).
+* **Read users** (_not implemented_).
+"""
+
+app = FastAPI(
+    title="ChimichangApp",
+    description=description,
+    summary="Deadpool's favorite app. Nuff said.",
+    version="0.0.1",
+    terms_of_service="http://example.com/terms/",
+    contact={
+        "name": "Deadpoolio the Amazing",
+        "url": "http://x-force.example.com/contact/",
+        "email": "dp@x-force.example.com",
+    },
+    license_info={
+        "name": "Apache 2.0",
+        "url": "https://www.apache.org/licenses/LICENSE-2.0.html",
+    },
+    lifespan=lifespan
+)
+
 
 
 @app.get("/")
